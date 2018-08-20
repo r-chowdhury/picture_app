2adb6d2552753e8feb9dab24fc442207d854a09a
Ryhan Chowdhury <ryhanchowdhury@apples-MacBook-Pro-2.local>
2018-08-20 17:20:22 -0500

    most popular method

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,10 +3,13 @@ class Tag < ApplicationRecord
   has_many :pictures, through: :picture_tags
 
   def self.most_popular
-    x = Tag.all.map do |tag|
-      tag.pictures.length
+    new_hash = Hash.new(0)
+    PictureTag.all.each do |picture_tag|
+      new_hash[picture_tag.tag] += 1
     end
+    x = new_hash.sort_by {|k,v| v}
+    x_reversed = x.reverse
 
-     return x.max
+    return [x_reversed[0], x_reversed[1], x_reversed[2]]
   end
 end
