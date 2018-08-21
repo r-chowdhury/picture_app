c67779d20abce0c3735d16548099c07ae1141156
Ryhan Chowdhury <ryhanchowdhury@apples-MacBook-Pro-2.local>
2018-08-21 13:29:11 -0500

    Adding class and instance methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_many :pictures
   has_many :comments, through: :pictures
+
+  def received_comments
+    #should return all the comments made
+    #on a picture that the user posted
+    users_pictures = Picture.all.select {|picture| picture.user == self}
+    return users_pictures.map {|user_picture| user_picture.comments}
+  end
 end
