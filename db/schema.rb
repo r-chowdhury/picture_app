bd112f137c85e82dbc5d8a6685a943a95bbd0980
Ryhan Chowdhury <ryhanchowdhury@apples-MacBook-Pro-2.local>
2018-08-22 13:48:27 -0500

    adding user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2018_08_20_203053) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
