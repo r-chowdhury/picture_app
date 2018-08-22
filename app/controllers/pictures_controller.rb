2b6276f3c23750b5e5b07175c84e279fbf3c9cc3
RebGov <becci.govert@gmail.com>
2018-08-22 15:52:54 -0500

    user able to add comment to picture on picture showpage

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,10 +1,15 @@
 class PicturesController < ApplicationController
   before_action :selected_picture, only: [:show, :edit, :update, :destroy]
+  helper_method :selected_picture
   def index
     @pictures = Picture.all.order("created_at DESC")
   end
 
   def show
+    @comment = Comment.new
+    @comment.picture_id = selected_picture.id
+    @comments = selected_picture.comments.all
+    session[:picture_id] = selected_picture.id
   end
 
   def new
@@ -20,7 +25,7 @@ class PicturesController < ApplicationController
   def picture_params
     params.require(:picture).permit(:image_url, :title, :user_id)
   end
-  
+
   def selected_picture
     @picture = Picture.find(params[:id])
   end
